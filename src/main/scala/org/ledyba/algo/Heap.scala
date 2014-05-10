8836ea074f52b52fc4b04ffe01afe95253dbcd49
psi <ledyba@users.sourceforge.jp>
2014-05-10 12:13:24 +0900

    codeJamとかいろいろ

--- src/main/scala/org/ledyba/algo/Heap.scala
+++ src/main/scala/org/ledyba/algo/Heap.scala
@@ -2,8 +2,9 @@ package org.ledyba.algo
 
 import scala.util.Random
 
-class Heap(val size:Int) {
-	val heap = Range(0, size).toArray;
+class Heap(val heap:Array[Int]) {
+	def this(size_ : Int) = { this(Range(0,size_).toArray); randomize(); }
+	var size = heap.size;
 	def swap(i:Int,j:Int):Unit = {
 		val t = heap(i);
 		heap(i) = heap(j);
@@ -15,25 +16,25 @@ class Heap(val size:Int) {
 			swap(i, r.nextInt(size-i)+i);
 		}
 	}
-	randomize()
 	def leftIdx(j:Int)  = (((j+1) << 1) | 0)-1;
 	def rightIdx(j:Int) = (((j+1) << 1) | 1)-1;
+	def parentIdx(j:Int) = ((j+1)/2) - 1;
 	def isMaxHeap:Boolean = {
 		def isMaxHeapImpl(i:Int):Boolean = {
 			val l = leftIdx(i);
 			val r = rightIdx(i);
-			(size <= l || (heap(i) > heap(l) && isMaxHeapImpl(l) )) &&
-			(size <= r || (heap(i) > heap(r) && isMaxHeapImpl(r) ))
+			(size <= l || (heap(i) >= heap(l) && isMaxHeapImpl(l) )) &&
+			(size <= r || (heap(i) >= heap(r) && isMaxHeapImpl(r) ))
 		}
 		isMaxHeapImpl(0);
 	}
 	def maxHeapify(i:Int):Unit = {
 		val l = leftIdx(i);
 		val r = rightIdx(i);
-		if( l < size && heap(l) > heap(i) && (r >= size || heap(l) > heap(r)) ) {
+		if( l < size && heap(l) > heap(i) && (r >= size || heap(l) >= heap(r)) ) {
 			swap(l, i);
 			maxHeapify(l);
-		}else if( r < size && heap(r) > heap(i) && (l >= size || heap(r) > heap(l)) ) {
+		}else if( r < size && heap(r) > heap(i) && (l >= size || heap(r) >= heap(l)) ) {
 			swap(r, i);
 			maxHeapify(r);
 		}
@@ -41,6 +42,34 @@ class Heap(val size:Int) {
 	def build():Unit={
 		for( i <- (0 to size/2).reverse ) {
 			maxHeapify(i)
+		}
+	}
+	def pop() = {
+		val it = heap.head;
+		heap(0) = heap(size-1)
+		size = size - 1;
+		maxHeapify(0)
+		it
+	}
+	def max() = heap.head;
+	def insert(v:Int) = {
+		if( size >= heap.size ) {
+			throw new Exception();
+		}
+		heap(size) = v;
+		var j = size;
+		size = size + 1;
+		while( j > 0 && heap(j) > heap(parentIdx(j)) ){
+			swap(j, parentIdx(j));
+			j = parentIdx(j);
+		}
+	}
+	def increase(i:Int, v:Int) = {
+		heap(i) = v;
+		var j = i;
+		while( j > 0 && heap(j) > heap(parentIdx(j)) ){
+			swap(j, parentIdx(j));
+			j = parentIdx(j);
 		}
 	}
 }
--- src/main/scala/org/ledyba/algo/Main.scala
+++ src/main/scala/org/ledyba/algo/Main.scala
@@ -1,19 +1,40 @@
 package org.ledyba.algo
 
 import scala.util.Random
+import java.io.BufferedInputStream
+import java.io.FileInputStream
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import scala.util.matching.Regex
+import scala.util.matching.Regex
+import org.junit.Assert
+import Function.tupled;
 
 object Main {
+	def shuffle(array:Array[Int]):Array[Int] = {
+		val r = new Random();
+		def swap( i:Int, j:Int ) = {
+			val t = array(i);
+			array(i) = array(j);
+			array(j) = t;
+		}
+		for(i <- Range(0, array.length-1)){
+			swap(i, r.nextInt(array.length-i)+i);
+		}
+		array
+	}
 	def main(args:Array[String]):Unit = {
 		val r = new Random();
-		val dat = (Array.fill(10)(0)).map(x => r.nextInt(10));
-		println( bubbleSort(dat).mkString(",") )
-		println( insertionSort(dat).mkString(",") )
-		println( mergeSort(dat).mkString(",") )
-		println( quickSort(dat).mkString(",") )
-		
-		val heap = new Heap(1000);
-		println( heap.isMaxHeap + "->" + { heap.build(); heap.isMaxHeap } )
-		
+		val dat = shuffle(Range(0, 10).toArray)
+		println( "bubble  : " + bubbleSort(dat).mkString(",") )
+		println( "insert  : " + insertionSort(dat).mkString(",") )
+		println( "merge   : " + mergeSort(dat).mkString(",") )
+		println( "quick   : " + quickSort(dat).mkString(",") )
+		println( "heap    : " + heapSort(dat).mkString(",") )
+		println( "counting: " + countingSort(dat, dat.length-1).mkString(",") )
+		println( "bucket  : " + bucketSort(dat, dat.length-1).mkString(",") )
+		//codeJam();
 	}
 	def swap(dat:Array[Int], i:Int, j:Int):Unit = {
 		val t = dat(i);
@@ -96,5 +117,112 @@ object Main {
 		}
 		quickSortI(dat, 0, dat.length-1)
 		dat
+	}
+	def heapSort(dat_ : Array[Int]):Array[Int] = {
+		val dat = dat_.clone().asInstanceOf[Array[Int]];
+		val heap = new Heap(dat);
+		heap.build();
+		for( i <- Range(0, dat.size).reverse ) {
+			val t = heap.pop();
+			dat(i) = t;
+		}
+		dat
+	}
+	def countingSort(dat_ : Array[Int], max:Int):Array[Int] = {
+		val dat = new Array[Int](dat_.size);
+		val cnt = (new Array[Int](max+1)).map(x => 0);
+		dat_.foreach(i => cnt(i) = cnt(i) + 1)
+		Stream.from(1).zip(cnt.tail).foreach( tupled { (i,d) => cnt(i) = cnt(i-1)+d } )
+		dat_.foreach(x => {cnt(x) = cnt(x)-1;dat(cnt(x)) = x;} )
+		dat
+	}
+	def bucketSort(dat_ : Array[Int], max:Int):Array[Int] = {
+		val dat = new Array[Int](dat_.size);
+		val n = Math.log(dat_.length).toInt;
+		val bucket = new Array[List[Int]](n);
+		def insertTo(lst : List[Int], v:Int):List[Int] = {
+			if(lst == null){
+				v :: List[Int]();
+			}else if(lst.isEmpty) {
+				v :: lst
+			}else if( lst.head > v ){
+				v :: lst
+			}else{
+				lst.head :: insertTo(lst.tail, v)
+			}
+		}
+		dat_.foreach(i =>{
+			val idx = n * i / (max+1);
+			bucket(idx) = insertTo( bucket(idx), i )
+		})
+		def allEmpty = bucket.foldLeft(true)( (b,x) => b && (x == null || x.isEmpty) )
+		var c = 0;
+		while( !allEmpty ) {
+			val (i, lst) = Stream.from(0).zip(bucket).minBy(tupled { (_, l) => if(l == null || l.isEmpty) max+1 else l.head } )
+			bucket(i) = lst.tail
+			dat(c) = lst.head
+			c = c+1;
+		}
+		dat
+	}
+	def codeJam():Unit={
+		val f = new BufferedReader(new InputStreamReader(new FileInputStream("A-large-practice.in.txt")))
+		val n = Integer.valueOf(f.readLine());
+		val dat = new Array[Array[String]](n);
+		for( i <- Range(0,n) ){
+			val d = new Array[String](Integer.valueOf(f.readLine()));
+			for( j <- Range(0,d.size) ){
+				d(j) = f.readLine();
+			}
+			dat(i) = d;
+		}
+		f.close();
+		def reduce(str:String)={
+			var last = str.head;
+			var cont = 1;
+			var lst = List[(Int, Char)]();
+			val b = new StringBuilder();
+			b.append(str.head);
+			for( c <- str.tail ) {
+				if( last != c ){
+					lst = (cont, last) :: lst;
+					b.append(last);
+					last = c;
+					cont = 1;
+				}else{
+					cont = cont+1;
+				}
+			}
+			lst = (cont, last) :: lst;
+			b.append(last);
+			(b.toString(), lst.reverse.toArray)
+		}
+		for( (i, pr) <- Stream.from(0).zip(dat) ){
+			val reduced = new Array[(String, Array[(Int, Char)])](pr.size)
+			for ((i,it) <-Stream.from(0).zip(pr)) {
+				reduced(i) = reduce(it)
+			}
+			if( reduced.tail.foldLeft(true)((b,s) => b && s._1 == reduced(0)._1 ) ){
+				var total = 0;
+				for( i <- Range(0, reduced.head._2.size) ) {
+					val max = reduced.maxBy(t => t._2(i)._1)._2(i)._1
+					val min = reduced.minBy(t => t._2(i)._1)._2(i)._1
+					var m = 10000;
+					for( c <- (min to max) ) {
+						var s = 0;
+						for( (_, cmp) <- reduced ){
+							s = s + Math.abs(cmp(i)._1 - c)
+						}
+						if( s < m ) {
+							m = s;
+						}
+					}
+					total = total + m;
+				}
+				println("Case #%d: %d".format(i+1, total));
+			}else{
+				println("Case #%d: Fegla Won".format(i+1));
+			}
+		}
 	}
 }
